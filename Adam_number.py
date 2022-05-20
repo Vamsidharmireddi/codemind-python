3c2db5ef1dcadd1c2661a4b740a1709f1b00cbd8
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-05-20 15:48:01 +0530

    n=int(input())##12 m=n*n##144 temp=n r=0 while n>0:     d=n%10     r=r*10+d     n=n//10 k=r*r##441 org=k r1=0 while k>0:     rem=k%10     r1=r1*10+rem     k=k//10 if r1==m:     print('True') else:     print('False')

---
+++ Adam_number.py
@@ -0,0 +1,20 @@
+n=int(input())##12
+m=n*n##144
+temp=n
+r=0
+while n>0:
+    d=n%10
+    r=r*10+d
+    n=n//10
+k=r*r##441
+org=k
+r1=0
+while k>0:
+    rem=k%10
+    r1=r1*10+rem
+    k=k//10
+if r1==m:
+    print('True')
+else:
+    print('False')
+
