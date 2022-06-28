2bb713777cf090eb35e4d286f7020f24cc323a03
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-28 23:00:31 +0530

    a,b=map(int,input().split()) for i in range(1,a+1):     if a%i==0 and b%i==0:         hcf=i print(a*b//hcf)

--- LCM.py
+++ LCM.py
@@ -1,5 +1,5 @@
 a,b=map(int,input().split())
-for i in range(a,a*b+1):
-    if i%a==0 and i%b==0:
-        break
-print(i)
+for i in range(1,a+1):
+    if a%i==0 and b%i==0:
+        hcf=i
+print(a*b//hcf)
