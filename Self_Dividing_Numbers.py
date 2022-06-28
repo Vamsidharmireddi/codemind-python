8cdf9f9bf83fb0d95df8acb3d098f14c2203d75d
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-28 21:28:39 +0530

    def is_self(n):     temp=n     c=0     k=0     while n>0:         d=n%10         c=c+1         if d!=0:             if temp%d==0:                 k=k+1         n=n//10     if c==k:         return 1     else:         return 0 a=int(input()) b=int(input()) for i in range(a,b+1):     if is_self(i):         print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,15 +1,20 @@
+def is_self(n):
+    temp=n
+    c=0
+    k=0
+    while n>0:
+        d=n%10
+        c=c+1
+        if d!=0:
+            if temp%d==0:
+                k=k+1
+        n=n//10
+    if c==k:
+        return 1
+    else:
+        return 0
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    k=0
-    c=0
-    temp=i
-    while i>0:
-        d=i%10
-        k=k+1
-        if d!=0:
-            if temp%d==0:
-                c=c+1
-        i=i//10
-    if k==c:
-        print(temp,end=' ')
+    if is_self(i):
+        print(i,end=' ')
