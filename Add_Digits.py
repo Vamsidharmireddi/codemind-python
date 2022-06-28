73cbba7eddb3c001c89c861ef9084f2a4007665e
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-28 21:22:46 +0530

    def add(n):     s=0     while n>0:         d=n%10         s=s+d         n=n//10     if s>0 and s<9:         return s     else:         return add(s) n=int(input()) print(add(n))

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,15 +1,12 @@
-def is_sum(n):
+def add(n):
     s=0
-    while n:
+    while n>0:
         d=n%10
         s=s+d
         n=n//10
-    if s<=9:
+    if s>0 and s<9:
         return s
     else:
-        n=is_sum(s)
-        return n
+        return add(s)
 n=int(input())
-print(is_sum(n))
-
-    
+print(add(n))
