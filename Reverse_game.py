b31d99b0da6ad359f0846cc7e56b95dc8a34ae18
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-30 10:39:05 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     r=0     while i>0:         d=i%10         r=r*10+d         i=i//10     print(r,end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,10 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range(0,len(a)):
-    s=0
-    while a[i]!=0:
-        d=a[i]%10
-        s=s*10+d
-        a[i]=a[i]//10
-    print(s,end=' ')
+c=0
+for i in a:
+    r=0
+    while i>0:
+        d=i%10
+        r=r*10+d
+        i=i//10
+    print(r,end=' ')
     
