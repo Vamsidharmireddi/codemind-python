a047f775337ffde58968040842f37b571e2ab814
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-30 11:38:34 +0530

    def is_prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) c=0 s=0 for i in range(0,len(a)):     if is_prime(a[i]):         c=c+1         s=s+a[i] avg=s/c print('{:.2f}'.format(avg))

--- Average_of_primes.py
+++ Average_of_primes.py
@@ -1,14 +1,20 @@
+def is_prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
 n=int(input())
 a=list(map(int,input().split()))
 c=0
 s=0
 for i in range(0,len(a)):
-    if a[i]!=1:
-        for j in range(2,int(a[i]**0.5)+1):
-            if(a[i]%j==0):
-                break
-        else:
-            c=c+1
-            s=s+a[i]
+    if is_prime(a[i]):
+        c=c+1
+        s=s+a[i]
 avg=s/c
-print("{:.2f}".format(avg))
+print('{:.2f}'.format(avg))
+
+        
