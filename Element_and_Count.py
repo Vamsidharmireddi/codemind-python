48d26466570f7d39e578cd0c29cb115e3e30b542
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-07-01 23:05:33 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 for i in range(0,len(a)):     c=1     for j in range(0,len(a)):         if i!=j:             if a[i]==a[j]:                 c=c+1                 a[j]=0     if a[i]!=0:         print(a[i],c,end=' ')

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,12 +1,12 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range(0,n):
+s=0
+for i in range(0,len(a)):
     c=1
-    for j in range(0,n):
-        if a[i]==a[j] and i!=j:
-            c=c+1
-            a[j]=0
+    for j in range(0,len(a)):
+        if i!=j:
+            if a[i]==a[j]:
+                c=c+1
+                a[j]=0
     if a[i]!=0:
         print(a[i],c,end=' ')
-
-        
