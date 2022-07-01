06fab04bf04631551544069b31459d49fe5a0a3c
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-07-01 22:39:20 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 for i in range(0,len(a)):     c=0     for j in range(0,len(a)):         if i!=j:             if a[i]==a[j]:                 a[j]=0     if a[i]%2==1:             s=s+a[i] print(s)

--- unique_odd_elements_sum.py
+++ unique_odd_elements_sum.py
@@ -1,11 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
 s=0
-for i in range(0,n):
-    for j in range(0,n):
-        if a[i]==a[j] and i!=j:
-            a[j]=0
+for i in range(0,len(a)):
+    c=0
+    for j in range(0,len(a)):
+        if i!=j:
+            if a[i]==a[j]:
+                a[j]=0
     if a[i]%2==1:
-        s=s+a[i]
+            s=s+a[i]
 print(s)
-        
+    
+    
