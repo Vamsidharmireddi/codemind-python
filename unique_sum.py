5939b8774718fc058add19ffd9c8f7e06c8b1115
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-07-01 22:57:48 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 for i in range(0,len(a)):     for j in range(0,len(a)):         if i!=j:             if a[i]==a[j]:                 a[j]=0     s=s+a[i] print(s)

--- unique_sum.py
+++ unique_sum.py
@@ -1,14 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
-k=0
 s=0
-for i in range(0,n):
-    for j in range(0,n):
-        if a[i]==a[j] and i!=j:
-            a[j]=0
-    else:
-        s=s+a[i]
+for i in range(0,len(a)):
+    for j in range(0,len(a)):
+        if i!=j:
+            if a[i]==a[j]:
+                a[j]=0
+    s=s+a[i]
 print(s)
-
-
-        
