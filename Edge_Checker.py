1c2d1a9262fbd26f20ef40749da7587bd0cc25bc
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-08-29 12:46:25 +0530

    a,b=map(int,input().split()) if (a==10 and b==1) or (b==10 and a==1):     print('Yes') elif a==b+1 or b==a+1:     print('Yes') else:     print('No')

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -1,9 +1,7 @@
-x,y=map(int,input().split())
-if y==x+1 or x==y+1:
+a,b=map(int,input().split())
+if (a==10 and b==1) or (b==10 and a==1):
     print('Yes')
-elif x==10 and y==1:
-    print('Yes')
-elif x==1 and y==10:
+elif a==b+1 or b==a+1:
     print('Yes')
 else:
     print('No')
