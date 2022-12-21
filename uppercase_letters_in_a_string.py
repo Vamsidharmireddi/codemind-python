cffb5a024ff2510fc6a4cff7a92c0fbe1f913510
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-12-21 11:04:03 +0530

    a=input() k=a.upper() c=0 for i in range(0,len(a)):     if a[i]==k[i] and a[i]!=" ":         c=c+1 print(c)

--- uppercase_letters_in_a_string.py
+++ uppercase_letters_in_a_string.py
@@ -1,7 +1,7 @@
-n=input()
-#print(n)
+a=input()
+k=a.upper()
 c=0
-for i in n:
-    if i==i.upper() and i!=' ':
-        c+=1
+for i in range(0,len(a)):
+    if a[i]==k[i] and a[i]!=" ":
+        c=c+1
 print(c)
